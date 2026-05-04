a905c3daf0457a811b0709ec33780f64de2eacaa
x44annie <kiyuuwithlove@gmail.com>
2026-05-04 12:04:38 +0200

    ADD: Basic spring 4.0 structure

---
+++ src/main/java/com/note/tiprover/TiproverWebApplication.java
@@ -0,0 +1,13 @@
+package com.note.tiprover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiproverWebApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(TiproverWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/note/tiprover/TiproverWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.note.tiprover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiproverWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
