f015e7d66a5051ac1e7ada4b381897aa6bb2d0c8
x44annie <kiyuuwithlove@gmail.com>
2026-05-04 12:44:30 +0200

    ADD: Column DeletedAt

--- src/main/java/com/note/tiprover/entity/Base.java
+++ src/main/java/com/note/tiprover/entity/Base.java
@@ -3,14 +3,16 @@ package com.note.tiprover.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 @EntityListeners({AuditingEntityListener.class})
+@SQLDelete(sql = "UPDATE users SET deletedAt = NOW() WHERE id = ?")
+@FilterDef(name = "deletedFilter", defaultCondition = "deletedAt IS NULL")
+@Filter(name = "deletedFilter")
 public abstract class Base {
 
     @CreationTimestamp
@@ -20,4 +22,7 @@ public abstract class Base {
     @UpdateTimestamp
     @Column(nullable = false)
     private LocalDateTime updatedAt;
+
+    @Column(name = "deleteAt")
+    private LocalDateTime deletedAt;
 }
